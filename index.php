7eb25eb69cdf9406380ce7342a4e57746ee9c2bd
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-04-30 06:13:27 +0200

    php new

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+// Include the config file
+// require_once 'config.php';
+
+// $_POST -> super global var
+if (!empty($_POST["submit__button"])) {
+    echo $_POST["username"];
+    echo $_POST["comment"];
+}
+
+// DB connection
+// try {
+//     $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+// } catch (PDOException $e) {
+//     echo "Something went wrong: " . $e->getMessage();
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP forum</title>
+</head>
+<body>
+  <h1 class="title">PHP forum</h1>
+  <div class="forum__wrapper__container">
+    <section>
+      <article>
+        <div class="test__article_wrapper">
+          <span>Name: </span>
+          <p classname="test__username">Hiro</p>
+          <time>04/29/23(Sat)05:17:58</time>
+        </div>
+        <p class="test__comment"></p>
+      </article>
+    </section>
+    <form class="forum__wrapper" method="POST">
+      <div>
+        <input type="submit" value="Input post" name="submit__button">
+        <label for="">Name: </label>
+        <input type="text" name="username">
+      </div>
+      <div>
+        <textarea class="comment__text__area" name="comment" id="" cols="30" rows="10"></textarea>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
